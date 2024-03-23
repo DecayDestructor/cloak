6ee719f5954dee21f5322b520533dd1bdfca6d43
Decay_Destructor <aaryanmantri29@gmail.com>
2024-03-23 13:24:22 +0530

    navbar bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,22 @@
 import { navbarData } from '../data/navbar'
 import logoDark from '../assets/logo-dark.png'
 import logoLight from '../assets/logo-light.png'
-import { Heart, LucideShoppingBag, Moon, Search, User } from 'lucide-react'
+import {
+  Heart,
+  LucideShoppingBag,
+  Moon,
+  Search,
+  ShoppingBag,
+  User,
+} from 'lucide-react'
+import { IconButton, Tooltip } from '@mui/material'
 
 const Navbar = ({ darkMode }) => {
   return (
-    <div className={` ${darkMode && 'dark'}`}>
-      <nav className="flex justify-between items-center px-5 py-5 bg-gray-100 dark:bg-black dark:text-white transition-colors duration-700 ease-out">
+    <div className={`${darkMode && 'dark'}`}>
+      <nav className="flex justify-between items-center px-5 py-5 bg-gray-100 dark:bg-black dark:text-white text-black transition-colors duration-700 ease-out">
         <img
-          src={darkMode ? logoDark : logoLight}
+          src={darkMode ? logoLight : logoDark}
           alt="logo-pic"
           className="h-[50px] w-[50px]"
         />
@@ -20,27 +28,45 @@ const Navbar = ({ darkMode }) => {
             </li>
           ))}
         </ul>
-        <div className="rounded-md p-2 flex items-center justify-center gap-2 bg-gray-100">
+        <div className="rounded-md p-2 flex items-center justify-center gap-2 bg-transparent">
           <form className="flex items-center bg-transparent gap-3 justify-center">
             <input
-              className="outline-none px-3 bg-transparent"
+              className={`outline-none px-3 bg-transparent dark:placeholder-white placeholder-black`}
               placeholder="Search for clothing"
             />
             <button type="submit" onClick={(e) => e.preventDefault()}>
-              <Search className={`${!darkMode && 'text-black'}`} />
+              <Search className={`${darkMode ? 'text-white' : 'text-black'}`} />
             </button>
           </form>
         </div>
         <div className="flex gap-10 justify-around items-center pr-6">
-          <a href="">
-            <User radius={'100%'} />
-          </a>
-          <a href="">
-            <Heart />
-          </a>
-          <a href="">
-            <LucideShoppingBag />
-          </a>
+          <Tooltip title="Profile">
+            <a href="">
+              <IconButton
+                style={darkMode ? { color: 'white' } : { color: 'black' }}
+              >
+                <User radius={'100%'} />
+              </IconButton>
+            </a>
+          </Tooltip>
+          <Tooltip title="Wishlist">
+            <a href="">
+              <IconButton
+                style={darkMode ? { color: 'white' } : { color: 'black' }}
+              >
+                <Heart radius={'100%'} />
+              </IconButton>
+            </a>
+          </Tooltip>
+          <Tooltip title="Cart">
+            <a href="">
+              <IconButton
+                style={darkMode ? { color: 'white' } : { color: 'black' }}
+              >
+                <LucideShoppingBag radius={'100%'} />
+              </IconButton>
+            </a>
+          </Tooltip>
         </div>
       </nav>
     </div>
