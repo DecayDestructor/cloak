9690b3c65a7fb21aa1a88b0a817dd50648a275ea
Decay_Destructor <aaryanmantri29@gmail.com>
2024-04-01 17:18:26 +0530

    gugugaga

--- src/sections/Carousel.jsx
+++ src/sections/Carousel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import landingMain from '../assets/landingMain.mp4'
 const Carousel = () => {
   return (
-    <div className="flex justify-center items-center mt-[-25px]">
+    <div className="flex justify-center items-center lg:mt-[-6.5%] md:mt-10 max-sm:mt-[100px]">
       <video autoPlay loop src={landingMain} muted className="grow" />
     </div>
   )
